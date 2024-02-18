7c64b2328ba9ccbabf7ee50d43c3e994ba7ba2b1
AwaNdioneWade <wadendioneawa@gmail.com>
2024-02-18 14:22:01 +0000

    ajout Id categorie

--- models/produit.js
+++ models/produit.js
@@ -7,6 +7,7 @@ const produitSchema = mongoose.Schema({
   description: { type: String, required: true },
   quantite: { type: Number, required: true },
   categorie: { type: String, required: true },
+  categorieId: { type: String, required: true },
   carracteristique: { type: String, required: true },
   prix: { type: Number, required: true },
   couleur: { type: String, required: true },
