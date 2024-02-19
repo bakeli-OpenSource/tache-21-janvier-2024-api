3ca403f07ef02b868d75605db81ba92df838520c
AwaNdioneWade <wadendioneawa@gmail.com>
2024-02-19 09:55:44 +0000

    categorie

--- models/categories.js
+++ models/categories.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const categorieSchema = mongoose.Schema({
   nom: { type: String, required: true },
-  imageUrl: { type: String, required: true },
   quantite: { type: Number, required: true }
 });
 
