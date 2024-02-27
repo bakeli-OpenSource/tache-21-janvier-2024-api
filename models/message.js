41c22c929219e82e4fb7021cfdcb2e68233323de
“Mbaye <seckm105@gmail.com>
2024-02-27 14:53:13 +0000

    modele message

--- models/message.js
+++ models/message.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const messageSchema = mongoose.Schema({
   prenomNom: { type: String, required: true },
-  telephone: { type: Number, required: true },
+  telephone: { type: String, required: true },
   email: { type: String, required: true },
   message: { type: String, required: true },
   date: { type: Date, default: Date.now },
