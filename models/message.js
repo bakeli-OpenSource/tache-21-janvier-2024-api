0fa01a17dc5cd18ee7d4fe7094eae16da4d06514
“Mbaye <seckm105@gmail.com>
2024-02-28 12:09:02 +0000

    Vu message

--- models/message.js
+++ models/message.js
@@ -7,7 +7,6 @@ const messageSchema = mongoose.Schema({
   message: { type: String, required: true },
   date: { type: Date, default: Date.now },
   lu: { type: Boolean, default: false },
-  vu: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('Message', messageSchema);
