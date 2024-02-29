8896d7eb741cafdc961350608580966565004ebc
“Mbaye <seckm105@gmail.com>
2024-02-29 09:20:25 +0000

    Qlq reglage modèle client

--- models/client.js
+++ models/client.js
@@ -6,7 +6,7 @@ const clientSchema = mongoose.Schema({
   nom: { type: String, required: true },
   telephone: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  adresse: { type: String, required: true, unique: true },
+  adresse: { type: String, required: true },
   password: { type: String, required: true },
 });
 
